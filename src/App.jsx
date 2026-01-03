fc7b03e4c55e84d977217cd1b1a9d92688f66e12
Jedidiah Staley <jedstaley@proton.me>
2026-01-03 16:36:11 -0500

    Added Home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,20 @@
-  import { useState } from 'react'
-  import './App.css'
+import { useState } from 'react'
+import './App.css'
 
-  import Header from './Components/Sections/Header';
-  import Footer from './Components/Sections/Footer';
+import Header from './Components/Sections/Header';
+import Footer from './Components/Sections/Footer';
+import Home from './Components/Pages/Home';
 
-  function App() {
-    return (
-      <div className="app">
-        <Header />
-        <main className="content" />
-        <Footer />
-      </div>
-    );
-  }
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <main className="content">
+        <Home />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
-  export default App
+export default App
---
+++ src/Components/Pages/Home.jsx
@@ -0,0 +1,11 @@
+import './Home.css';
+
+function Home() {
+    return (
+      <section className="home">
+        <p>Welcome to Conover Gospel Fellowship</p>
+      </section>
+    );
+}
+
+export default Home;
